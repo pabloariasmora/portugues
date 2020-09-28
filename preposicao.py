ff3631d13627ef313f83075eb9027e0245ee4154
Arias Mora <jpmora@3c22fb25bf16.ant.amazon.com>
2020-09-27 21:29:23 -0600

    Bug

--- preposicao.py
+++ preposicao.py
@@ -10,11 +10,17 @@ random.shuffle(questions)
 general_question = 'Complete com a preposiçâo, artigo e/ou contrações:'
 
 total = len(questions)
+good = 0
+bad = 0
 for row in questions:
     print(Fore.BLUE + general_question + Style.RESET_ALL)
     print(row[0])
     answer = input("reponda: ")
-    validate_answer(answer, row[1])
 
-    print()
+    response = validate_answer(answer, row[1])
+    if response:
+        good = 1 + good
+    else:
+        bad = bad + 1
+    print('T[{}]:'.format(total)+Fore.GREEN + 'S[{}]:'.format(good)+Fore.RED + 'F[{}]'.format(bad) + Style.RESET_ALL)
     print('-------------------')
